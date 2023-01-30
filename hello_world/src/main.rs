2d4111b14594e8aa2a440704d2d861464efbd150
zyd <zydmayday@gmail.com>
2023-01-30 23:06:57 +0900

    iter

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,13 @@
+fn hello() {
+    let a = 'a';
+    let b = 'b';
+    let c = 'c';
+    let nums = [a, b, c];
+    for i in nums.iter() {
+        println!("{}", i)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    hello()
 }
